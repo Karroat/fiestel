47cfdf938bdd76fe666c84baf09214cfee40f8b4
Karroat <106026199+Karroat@users.noreply.github.com>
2022-06-22 22:01:57 +0530

    Update work.java

--- work.java
+++ work.java
@@ -22,3 +22,26 @@ int n=s.nextInt();
 System.out.println(generateRandomDigits(n));
 }
 }
+
+
+/*coverts decimal to binary*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Code
+{
+	static void decimalToBinary(int num)
+	{
+		System.out.println(Integer.parseInt(Integer.toBinaryString(num)));
+	}
+}
+
+class Demo1 extends Code
+{
+	public static void main(String[] args)
+	{
+            Scanner s=new Scanner(System.in);
+            int num= s.nextInt();
+	    decimalToBinary(num);
+	}
+}
