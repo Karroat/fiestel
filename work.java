4ce63dbdc9a894baf19edebb96a2c766783c709c
Karroat <106026199+Karroat@users.noreply.github.com>
2022-06-22 22:16:46 +0530

    Update work.java

--- work.java
+++ work.java
@@ -149,3 +149,40 @@ public static void main(String args[])
  }
 }
 }
+
+/* encryption*/
+import java.util.*;
+import java.util.UUID;
+
+class Encryptioo
+{
+    public String encrypt(int x[]) 
+  {
+        string l[]= new string[100];
+        string r[]= new string[100];
+        string temp;
+        int x = x.length() / 2;
+        String l[] = variable.substring(variable(x)); //some input binary string if stored in a string variable//
+        String r[] = variable.substring(variable(x));
+
+        for (int i= 1; i<3; i++)
+       {
+            String temp = r[i];
+            String funct[i] = function(r[i],k[i]);
+            r[i]= XOR(l[i], funct[i]);
+            l[i] = temp;
+       }
+        return l + "" + r;
+  public static string function(r[i],k[i])
+  {
+    if(r.length==k.length)
+     {
+       for(int i=1;i<r.length;i++)
+      {
+
+        system.out.print(logicalXOR(r[i],k[i]));
+
+      }
+   } 
+}  
+}
