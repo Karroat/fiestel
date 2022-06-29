6306161750e4659041b58833165f564c352b0129
AK0206 <96947561+AK0206@users.noreply.github.com>
2022-06-29 17:31:47 +0530

    Update work.java

--- work.java
+++ work.java
@@ -1,14 +1,16 @@
+	
 import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-class Demo19
+class Demo19 extends keys
 {
 public static void main(String args[])
 {  
    String s;
    Scanner sc = new Scanner(System.in);
    s=sc.nextLine();
+   s=s.toUpperCase();
    System.out.println(s);
    int x=s.length();
    int ascii[] = new int[s.length()]; 
@@ -19,7 +21,7 @@ public static void main(String args[])
        
      }
    System.out.println();
-   long k[];
+   //int k[];
    int l[];
    int r[];
    //int temp[];
@@ -53,107 +55,10 @@ public static void main(String args[])
      r[j] = ascii[i];
     }
    }
-   k = new long[r.length];
-   for(int i=0;i<r.length;i++)
-   {
-    k[i] = i; //Math.abs(UUID.randomUUID().getMostSignificantBits());
-   }
-   int funct[] = new int[r.length];
-   int temp[] = new int[r.length];
-     for(int j=0;j<k.length;j++) //exception
-     {
-        for (int i= 0,z=0; i<r.length && z<l.length; i++,z++)
-       {
-            temp[i] = r[i];
-            funct[i] =(int)(r[i]^k[j]);
-            r[i]= (int)(l[z]^funct[i]);
-            l[z]= temp[i];
-       }
-     }
-     for(int i= 0,j=0; i<r.length && j<l.length; i++,j++)
-     {
-      
-      System.out.println(l[j]+" "+r[i]);
-     }
-       
-
-}
-}
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-
-class Demo19
-{
-public static void main(String args[])
-{  
-   String s;
-   Scanner sc = new Scanner(System.in);
-   s=sc.nextLine();
-   System.out.println(s);
-   int x=s.length();
-   int ascii[] = new int[s.length()]; 
-   for(int i=0;i<s.length();i++)
-     {
-       ascii[i]= (int) s.charAt(i);
-       System.out.print(ascii[i]);
-       
-     }
-   System.out.println();
-   int k[];
-   int l[];
-   int r[];
-   //int temp[];
-   //int funct[];
-   if(x%2==0)
-   {
-    int n=(int) x/2;
-    l = new int[n];
-    r = new int[n];
-    for(int i=0;i<n;i++)
-    {
-     l[i] = ascii[i];
-    }
-    for(int i=n,j=0;i<x && j<n;i++,j++)
-    {
-     
-     r[j] = ascii[i];
-    }
-   }
-   else
-   {
-    int m=(int) x/2;
-    l = new int[m];
-    r = new int[m+1];
-    for(int i=0;i<m;i++)
-    {
-     l[i] = ascii[i];
-    }
-    for(int i=m-1,j=0;i<x && j<m;i++,j++)
-    {
-     r[j] = ascii[i];
-    }
-   }
-   for(int i=0;i<r.length();i++)
-   {
-    k[i] = Math.abs(UUID.randomUUID().getMostSignificantBits());
-   }
+   //for(int i=0;i<r.length();i++)
+  // {
+   //   k[i] = Math.abs(UUID.randomUUID().getMostSignificantBits());
+   //}
    int funct[] = new int[r.length];
    int temp[] = new int[r.length];
      for(int j=0;j<k.length;j++) //exception
@@ -238,32 +143,37 @@ import java.lang.*;
 import java.io.*;
 import java.util.UUID;
 
-class Expo
+class keys
 {
-   public static void countDigit(int r)
-  {
+   int n=2;
+   /*public static void countDigit(int r)
+   {
      int count = 0;
      while (r != 0) {
          r = r / 10;
          ++count;
-     }
+   }
      System.out.println(count);
      r=count;
-     System.out.println(generateRandomDigits(r));
-  }
-public static int generateRandomDigits(int r) 
-{
+     System.out.println(generateRandomDigits(r));*/
+   
+   public static int generateRandomDigits(int r) 
+   {
     int m = (int) Math.pow(10, r - 1);//math.pow(1arg,2arg) means 10 to the power of n-1//
-    return m + new Random().nextInt(9 * m);/*100000<=n<=900000*/ //means m is 10 to the power something//
-}
+    int x = m + new Random().nextInt(9 * m);/*100000<=n<=900000*/ //means m is 10 to the power something//
+    return x;
+   }
+   for(
 }
 class Demo15 extends Expo
 {
 public static void main(String args[])
 {
-Scanner s=new Scanner(System.in);
-int n=s.nextInt();
-countDigit(n);
+//Scanner s=new Scanner(System.in);
+//int n=s.nextInt();
+int n = 2;
+//countDigit(n);
+generateRandomDigits(n);
 }
 }
 */
