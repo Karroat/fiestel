7c3da3c7b82103659f80866801ab3f04056bde58
mahathimaddipatla <96947396+mahathimaddipatla@users.noreply.github.com>
2022-07-12 16:49:55 +0530

    Update work.java

--- work.java
+++ work.java
@@ -508,3 +508,43 @@ public class Soundl
        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.UUID;
+class keys
+{  
+   
+   static int generateRandomDigits() throws IOException
+   {
+    //Random r=new Random();
+    FileOutputStream fos=new FileOutputStream("keys.txt");
+    DataOutputStream dos=new DataOutputStream(fos);
+    FileInputStream fis=new FileInputStream("keys.txt");
+    DataInputStream dis=new DataInputStream(fis);
+    for(int i=0;i<10;i++)
+    {
+     long x=Math.abs(UUID.randomUUID().getMostSignificantBits());
+     dos.writeLong(x);
+     long y=dis.readLong();
+     System.out.println(y);
+    }
+    dis.close();    
+    dos.close();
+    return 0;
+   }
+   public static void main(String args[]) throws IOException
+   {
+    generateRandomDigits();
+   }
+}
