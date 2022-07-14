84866de46977dd5b84417b2a4bed6d8fb011e7e3
Akshit <96947561+AK0206@users.noreply.github.com>
2022-07-14 20:16:29 +0530

    Update work.java

--- work.java
+++ work.java
@@ -318,4 +318,55 @@ class keys
 
 
 
-//
+//***************************************** Soundl ****************************************
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.util.UUID;
+import java.io.FileReader;
+import java.io.FileReader.*;
+import javax.sound.sampled.*;
+
+public class Soundl
+{
+    public static float SAMPLE_RATE = 8000f;
+    public static void tone(int hz, int msecs) 
+    throws LineUnavailableException 
+    {
+        tone(hz, msecs, 1.0);
+    }
+
+    public static void tone(int hz, int msecs, double vol)
+    throws LineUnavailableException 
+    {
+        byte[] buf = new byte[1];
+        AudioFormat af = new AudioFormat(SAMPLE_RATE,8,1,true,false);     
+        SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
+        sdl.open(af);
+        sdl.start();
+        for (int i=0; i < msecs*8; i++)
+        {
+              double angle = i / (SAMPLE_RATE / hz) * 2.0 * Math.PI;
+              buf[0] = (byte)(Math.sin(angle) * 127.0 * vol);
+              sdl.write(buf,0,1);
+        }
+        sdl.drain();
+        sdl.stop();
+        sdl.close();
+    }
+      
+
+   public static void main(String[] args) throws Exception
+    {
+        FileReader fis = new FileReader("D:\\project\\en.txt");
+        BufferedReader bis = new BufferedReader(fis);
+        String s = bis.readLine();
+        int x = s.length();
+        //for (int i = 0; i < x ; ++i) 
+        //{
+	  // System.out.println("Beep : " + i);    // Sound d = new Sound();
+           Soundl.tone(7000,(440*x));
+	//}
+        bis.close();
+    }
+}
